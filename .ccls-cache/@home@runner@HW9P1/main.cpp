b09ecb52a8e455bfa4bcfe39055482d0bbe8e974
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-10-25 21:32:02 +0000

    Set up data

--- .ccls-cache/@home@runner@HW9P1/main.cpp
+++ .ccls-cache/@home@runner@HW9P1/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+
+//////////Funtion Prototypes///////////////
+double printMatrix[60][7];
+
 int main() {
+  double grade[60][7] = {
+                        {1, 100, 100, 100, 100},
+                        {2, 100, 0, 100, 0},
+                        {3, 82, 94, 73,86},
+                        {4, 64,74, 84, 94},
+                        {5, 94, 84, 74, 64}
+                        }
   
-}
+  
+  }
+
+

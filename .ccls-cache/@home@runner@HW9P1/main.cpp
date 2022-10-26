31ad00b42231933b0a8149b151b08965cdcbed03
AshlyndH <112569560+AshRinn@users.noreply.github.com>
2022-10-26 15:43:34 +0000

    seting up printmatrix

--- .ccls-cache/@home@runner@HW9P1/main.cpp
+++ .ccls-cache/@home@runner@HW9P1/main.cpp
@@ -3,26 +3,35 @@ using namespace std;
 
 
 //////////Funtion Prototypes///////////////
-double printMatrix[60][7];
+void printMatrix(int myMat[][7], int N_ROWS, int N_COLS);
 
 int main()
 {
-  double grade[60][7] = {
+  const int N_ROWS = 5;
+  const int N_COLS = 7;
+  double grade[N_ROWS][N_COLS] = 
+                        {
                         {1, 100, 100, 100, 100},
                         {2, 100, 0, 100, 0},
                         {3, 82, 94, 73, 86},
                         {4, 64,74, 84, 94},
                         {5, 94, 84, 74, 64}
                         };
-  for (int = 0; i < 60; i++)
+  printMatrix( grade, N_ROWS, N_COLS);
+
+  return 0;
+  }
+
+void printMatrix(int grade[][7], int N_ROWS, int N_COLS)
+
+  for (int = 0; i < 5; i++)
   {
       for (int n = 0; n < 7; n++)
       {
         grade[i][6] = (grade[i][1] * .2) + (grade[i][2]*.3) +(grade[i][3] * .3) + (grade[i][4] *.2)
       }  
   }
-  
-  
+
 }
 
 
